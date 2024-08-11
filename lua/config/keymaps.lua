21ba7304fa9af960afbe1bfe18ea19f0052b83e3
Jan Riethmayer <jan@riethmayer.de>
2024-08-11 22:00:43 +0100

    Add obsidian stuff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,65 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--------------
+-- obsidian --
+--------------
+--
+-- >>> oo # from shell, navigate to vault (optional)
+--
+-- # NEW NOTE
+-- >>> on "Note Name" # call my "obsidian new note" shell script (~/bin/on)
+-- >>>
+-- >>> ))) <leader>on # inside vim now, format note as template
+-- >>> ))) # add tag, e.g. fact / blog / video / etc..
+-- >>> ))) # add hubs, e.g. [[python]], [[machine-learning]], etc...
+-- >>> ))) <leader>of # format title
+--
+-- # END OF DAY/WEEK REVIEW
+-- >>> or # review notes in inbox
+-- >>>
+-- >>> ))) <leader>ok # inside vim now, move to zettelkasten
+-- >>> ))) <leader>odd # or delete
+-- >>>
+-- >>> og # organize saved notes from zettelkasten into notes/[tag] folders
+-- >>> ou # sync local with Notion
+local obsidian_vault = os.getenv("OBSIDIAN_VAULT") or "/Users/janriethmayer/obsidian/riethmayer"
+
+-- navigate to vault
+vim.keymap.set("n", "<leader>oo", ":cd " .. obsidian_vault .. "<cr>", { desc = "navigate to vault" })
+--
+-- convert note to template and remove leading white space
+vim.keymap.set(
+  "n",
+  "<leader>on",
+  ":ObsidianTemplate note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//]])<cr>",
+  { desc = "convert note to template" }
+)
+-- strip date from note title and replace dashes with spaces
+-- must have cursor on title
+vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>", { desc = "format title" })
+--
+-- search for files in full vault
+vim.keymap.set(
+  "n",
+  "<leader>os",
+  ':Telescope find_files search_dirs={"' .. obsidian_vault .. '/notes"}<cr>',
+  { desc = "search for files in full vault" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>zg",
+  ':Telescope live_grep search_dirs={"' .. obsidian_vault .. '/notes"}<cr>',
+  { desc = "grep for files in full vault" }
+)
+--
+-- search for files in notes (ignore zettelkasten)
+-- vim.keymap.set("n", "<leader>ois", ":Telescope find_files search_dirs={\"/Users/alex/library/Mobile\\ Documents/iCloud~md~obsidian/Documents/ZazenCodes/notes\"}<cr>")
+-- vim.keymap.set("n", "<leader>oiz", ":Telescope live_grep search_dirs={\"/Users/alex/library/Mobile\\ Documents/iCloud~md~obsidian/Documents/ZazenCodes/notes\"}<cr>")
+--
+-- for review workflow
+-- move file in current buffer to zettelkasten folder
+vim.keymap.set("n", "<leader>zk", ":!mv '%:p' " .. obsidian_vault .. "/zettelkasten<cr>:bd<cr>")
+-- delete file in current buffer
+vim.keymap.set("n", "<leader>zdd", ":!rm '%:p'<cr>:bd<cr>")
